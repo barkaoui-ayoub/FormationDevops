79dcda445e4b682b9c4a3b92b5d66613dceaa210
abarkaoui <abarkaoui@DESKTOP-U2N2Q0I.home>
2019-02-14 17:50:50 +0100

    First commit

---
+++ FormationDevops/src/main/java/com/devops/formation/FormationDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.devops.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationDevopsApplication.class, args);
+	}
+
+}
+
